150f5be6836244421191c73a2e72433f4c9fe6da
nellyl <nelly.lam@devinci.fr>
2017-02-02 17:52:13 +0100

    real-estate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require( 'express' );
 var request = require( 'request' );
 var cheerio = require( 'cheerio' );
 
+
 //creating a new express server
 var app = express();
 
@@ -20,6 +21,36 @@ app.get( '/', function ( req, res ) {
     });
 });
 
+//makes http calls
+request( 'https://www.leboncoin.fr/ventes_immobilieres/999621886.htm?ca=12_s', function ( error, response, html ) {
+    if ( !error && response.statusCode == 200 ) {
+        var $ = cheerio.load( html );
+        var prix = parseInt( $( 'h2.item_price span.value' ).get()[0].children[0].data.replace( /\s/g, '' ), 10 );
+        //parseInt : enlève tout, affiche seulement le nombre, data.replace(1,'') : remplace les 1 par rien
+        var ville = $( 'div.line.line_city span.value' ).get()[0].children[0].data.replace( /\s/g, '' );
+        var type = $( 'h2.clearfix span.value' ).get()[2].children[0].data.replace( /\s/g, '' );
+        var pièces = $( 'h2.clearfix span.value' ).get()[3].children[0].data.replace( /\s/g, '' );
+        var surface = parseInt( $( 'h2.clearfix span.value' ).get()[4].children[0].data.replace( /\s/g, '' ), 10 );
+        var ges = $( 'h2.clearfix span.value' ).get()[5].children[0].data.replace( /\s/g, '' );
+        var classe = $( 'h2.clearfix span.value' ).get()[6].children[0].data.replace( /\s/g, '' );
+        console.log( prix );
+        console.log( ville );
+        console.log( type );
+        console.log( pièces );
+        console.log( surface );
+        console.log( ges );
+        console.log( classe );
+    }
+
+});
+
+request( 'https://www.meilleursagents.com/prix-immobilier/courbevoie-92400/', function ( error, response, html ) {
+    if ( !error && response.statusCode == 200 ) {
+        var $ = cheerio.load( html );
+        var prix2 = parseInt( $( 'div.prices-summary_values div.prices-summary_cell--median' ).get( 1 ).children[0].data.replace( /\s/g, '' ), 10 );
+        console.log( prix2 * 80 );
+    }
+});
 
 //launch the server on the 3000 port
 app.listen( 3000, function () {
